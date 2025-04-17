307b14e8ee258fd6b579db099569a30efd57329a
sezar123312 <sezar554433@gmail.com>
2025-04-17 16:02:45 +0330

    Revert "First Commit"

--- main.cpp/main.cpp
+++ main.cpp/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
-void printHello() {
-    cout << "Hello, World" << endl;
-}
+
 void printStudentid() {
     cout << "My ID is : 4033633005 " << endl;
 }
 int main() {
-    printHello();
+   
     printStudentid();
     return 0;
 }
