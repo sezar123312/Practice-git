578e5aa77e30d0377b955870ec8d5a5aec530617
sezar123312 <sezar554433@gmail.com>
2025-04-17 15:51:25 +0330

    Second Commit

--- main.cpp/main.cpp
+++ main.cpp/main.cpp
@@ -3,8 +3,11 @@ using namespace std;
 void printHello() {
     cout << "Hello, World" << endl;
 }
-
+void printStudentid() {
+    cout << "My ID is : 4033633005 " << endl;
+}
 int main() {
     printHello();
+    printStudentid();
     return 0;
 }
